aa0d5e51115b807a162512546d9c12396cb97e16
MinghaoZhang <mzhang26@scienceleadership.org>
2026-03-24 15:03:22 -0400

    e

---
+++ src/Fibonacci.java
@@ -0,0 +1,24 @@
+public class Fibonacci implements Fibonaccilike {
+    // Fields
+    int amountofNumbersInSequence;
+    int number1;
+    int number2;
+
+    public Fibonacci(int number1, int number2, int amountofNumbersInSequence) {
+        this.number2 = number2;
+        this.number1 = number1;
+        this.amountofNumbersInSequence = amountofNumbersInSequence;
+    }
+
+    // Methods
+ public void calculate() {
+    for(int i = amountofNumbersInSequence; i > 0; i = i - 1 ) {
+     System.out.println(number2 + number1 );
+        int bob = number1 + number2;;
+    number1 = number2;
+     number2 = bob;
+
+
+    }
+    }
+}
---
+++ src/Fibonaccilike.java
@@ -0,0 +1,9 @@
+public interface    Fibonaccilike {
+    // Fields
+    int amountofNumbersInSequence = 0;
+    int number1 = 0;
+    int number2 = 0;
+
+    // Methods
+     void calculate();
+}
